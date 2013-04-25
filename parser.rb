cb894b5ed823ad29c9ea4272d2d8ee23461bd665
Robert Brook <mail@robertbrook.com>
2013-04-25 14:46:46 +0100

    scraping a spec

--- parser.rb
+++ parser.rb
@@ -9,62 +9,71 @@ require "yaml"
 
 class Parser
 
-target_link = ""
-forthcoming_business_page = Nokogiri::HTML(open("http://www.lordswhips.org.uk/display/templatedisplay1.asp?sectionid=7"))
-target_link = forthcoming_business_page.css('a').reverse.select {|link| link['href'].include?("http://www.lordswhips.org.uk/documents/") }
+  def initialize
+    target_link = ""
+    forthcoming_business_page = Nokogiri::HTML(open("http://www.lordswhips.org.uk/display/templatedisplay1.asp?sectionid=7"))
+    target_link = forthcoming_business_page.css('a').reverse.select {|link| link['href'].include?("http://www.lordswhips.org.uk/documents/") }
 
-io     = open(URI::encode(target_link[0]['href']))
-#pdf = PDF::Reader.new(io)
+    io     = open(URI::encode(target_link[0]['href']))
+    #pdf = PDF::Reader.new(io)
 
-pdf = PDF::Reader.new("FB 2013 03 27 r.pdf")
-mytext = ""
-business = {:dates => []}
+    @pdf = PDF::Reader.new("FB 2013 03 27 r.pdf")
+    @mytext = ""
+    @business = {:dates => []}
 
-pdf.pages.each do |page|
-	mytext << page.text
-end
+  end
 
-mytext.lines.each do |line|
+def process
+  @pdf.pages.each do |page|
+    @mytext << page.text
+  end
 
-	case line
+  @mytext.lines.each do |line|
 
-	when /Information/
-		break
+    case line
 
-	when /\b([A-Z]{2,}[DAY] \d.+)/
+    when /Information/
+      break
+
+    when /\b([A-Z]{2,}[DAY] \d.+)/
 		@dateflag = $1
 		@itemflag = ""
-		business[:dates] << {:date => @dateflag, :times => [], :note => ""}
+		@business[:dates] << {:date => @dateflag, :times => [], :note => ""}
 
 	when /^(\d)/
 		@itemflag = $1
 		# puts "\t\t" + line
 		# first line of item
-		target = business[:dates].select { |date|  date[:date] == @dateflag  }
+		target = @business[:dates].select { |date|  date[:date] == @dateflag  }
 		target[0][:times][0][:items] << {:item => line.strip}
 
 
 	when /^([A-Z])/
-		target = business[:dates].select { |date|  date[:date] == @dateflag  }
+		target = @business[:dates].select { |date|  date[:date] == @dateflag  }
 		target[0][:times] << {:time => line.strip, :items => []}
 
 	when /^\n$/
-		p
-		#when /^[    ]/
-	when
+      p
+      #when /^[    ]/
+    when
 
-		if @itemflag == ""
-			# not picking up everything correctly yet
-			target = business[:dates].select { |date|  date[:date] == @dateflag  }
-			target[0][:note] = line.strip
-		else
-			puts @itemflag.to_s + "\t\t" + line
-		end
+      if @itemflag == ""
+        # not picking up everything correctly yet
+        target = @business[:dates].select { |date|  date[:date] == @dateflag  }
+        target[0][:note] = line.strip
+      else
+        puts @itemflag.to_s + "\t\t" + line
+      end
 
-	end
+    end
+  end
+
+  
 end
 
-pp business
-
+def output
+	pp @business
+end
 
 end
+
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,14 +3,20 @@ require 'minitest/autorun'
 require './parser'
 
 describe Parser do
-	before do
+  before do
     @parser = Parser.new
   end
-  
+
   describe "when fired up" do
     it "must respond positively" do
-      @parser.reply.must_not_equal ""
+      @parser.mytext.must_equal ""
     end
+  end
+
+  describe "process" do
+  end
+
+  describe "output" do
   end
 
 end
