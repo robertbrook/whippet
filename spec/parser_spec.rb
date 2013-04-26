4e6b52b7dd69b3f6ad086f77d3433c935041f327
Robert Brook <mail@robertbrook.com>
2013-04-26 11:37:40 +0100

    slightly less shameful spec

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -14,16 +14,26 @@ describe Parser, "when given the Forthcoming Business for 27th March 2013 PDF as
       @parser.must_be_instance_of Parser
     end
 
-  end
+    it "must have an output" do
+      @parser.output.wont_be_empty
+    end
 
-  describe "having run the Parser process and Parser output it" do
+    it "must return eight pages" do
+      @parser.pages.length.must_equal 8
+    end
 
-  	it "must return an @parser with eight pages" do
-  		@parser.process
-  		@parser.output
-  		@parser.pages.length.must_equal 8
-  	end
+    it "must return 14 dates" do
+      @parser.output[:dates].length.must_equal 14
+    end
+
+    it "must have a first date of 'WEDNESDAY 27 MARCH 2013'" do
+      @parser.output[:dates][0][:date].must_equal "WEDNESDAY 27 MARCH 2013"
+    end
+
+    it "must have a last date of 'FRIDAY 17 MAY 2013'" do
+      @parser.output[:dates][-1][:date].must_equal "FRIDAY 17 MAY 2013"
+    end
+
   end
 
 end
-
