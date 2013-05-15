5fdeca0987187a241fbbe4b7d70ea31b6a9c728d
Robert Brook <mail@robertbrook.com>
2013-05-15 17:16:17 +0100

    simpler puller

--- puller.rb
+++ puller.rb
@@ -1,15 +1,16 @@
-require "open-uri"
+#require "open-uri"
 require "./parser"
-require "json"
+require "haml"
 
 Dir.glob('./PDFs/*.pdf') do |pdf|
   @parser = Parser.new(pdf)
   @parser.process
-  puts @parser.output.to_json
+  p @parser.output
 end
-
 
 #target_link = ""
 #forthcoming_business_page = Nokogiri::HTML(open("http://www.lordswhips.org.uk/display/templatedisplay1.asp?sectionid=7"))
 #target_link = forthcoming_business_page.css('a').reverse.select {|link| link['href'].include?("http://www.lordswhips.org.uk/documents/") }
 #io     = open(URI::encode(target_link[0]['href']))
+
+
