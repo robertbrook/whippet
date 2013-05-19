16b0604a1ae08428a8dfeeb1364bd323ca048da4
lizconlan <lizconlan@gmail.com>
2013-05-19 03:47:29 +0100

    scruffily dumping some db content onto the screen

--- server.rb
+++ server.rb
@@ -2,7 +2,12 @@ require 'sinatra'
 require 'mongoid'
 require './lib/parser'
 
+before do
+  Mongoid.load!("./config/mongo.yml")
+end
+
 get '/' do
-	@time = Time.now
+  @time = Time.now
+  @calendar_days = SittingDay.all.sort("date DESC").limit(4)
   haml :index
 end
