71bd500e7beac4bcea88a51ed043d8bd71191ae9
Robert Brook <mail@robertbrook.com>
2013-06-04 18:05:21 +0100

    needs wiring

--- server.rb
+++ server.rb
@@ -2,6 +2,9 @@ require 'sinatra'
 require 'mongoid'
 require './lib/parser'
 
+# to wire up with fake struct later
+# MONGOHQ_DEV_URI = ENV['MONGOHQ_DEV_URI'] || YAML::load(File.read("./config/mongo.yml"))[:websolr_url]
+
 before do
   Mongoid.load!("./config/mongo.yml")
 end
