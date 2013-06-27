5fb6cd1adb68e7c18bcbe4a6cb98811c51fc81fe
lizconlan <lizconlan@gmail.com>
2013-06-27 17:53:00 +0100

    starting to work on a diff() method for CalendarDay

--- models/calendar_day.rb
+++ models/calendar_day.rb
@@ -4,6 +4,21 @@ require "./lib/mm_monkeypatch"
 class CalendarDay
   include MongoMapper::Document
   
+  def diff(other)
+    change = {}
+    unless other.is_a?(CalendarDay)
+      raise "Unable to compare #{self.class} to #{other.class}"
+    end
+    #the easy bit
+    change[:note] = other.note if note != other.note
+    change[:_type] = other._type if _type != other._type
+    change[:accepted] = other.accepted if accepted != other.accepted
+    change[:is_provisional] = other.is_provisional if is_provisional != other.is_provisional
+    
+    change[:pdf_info] = other.pdf_info unless change.empty?
+    change
+  end
+  
   def becomes(klass)
     became = klass.new
     self.instance_variables.each do |var|
---
+++ spec/calendar_day_spec.rb
@@ -0,0 +1,32 @@
+#encoding: utf-8
+
+require './spec/minitest_helper.rb'
+require './models/calendar_day'
+
+class CalendarDayTest < MiniTest::Spec  
+  describe "CalendarDay" do
+    describe "in general" do
+      it "must be allow casting from CalendarDay to SittingDay" do
+        day = CalendarDay.new(:note => "test")
+        sitting_day = day.becomes(SittingDay)
+        sitting_day.must_be_kind_of SittingDay
+        sitting_day.note.must_equal "test"
+      end
+      
+      it "must be allow casting from CalendarDay to NonSittingDay" do
+        day = CalendarDay.new(:note => "test2")
+        sitting_day = day.becomes(NonSittingDay)
+        sitting_day.must_be_kind_of NonSittingDay
+        sitting_day.note.must_equal "test2"
+      end
+      
+      it "must throw an error when asked to diff with an object not derived from CalendarDay" do
+        day = SittingDay.new
+        compare_with_string = lambda { day.diff("test") }
+        compare_with_string.must_raise RuntimeError
+        error = compare_with_string.call rescue $!
+        error.message.must_equal("Unable to compare SittingDay to String")
+      end
+    end
+  end
+end
