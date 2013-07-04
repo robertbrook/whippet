082ee6f7005c1e324ee0c59e9ec31d16f14fa5a8
lizconlan <lizconlan@gmail.com>
2013-07-04 23:17:51 +0100

    added failing spec for the "Lord Freud 6" problem

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -221,8 +221,12 @@ class ParserTest < MiniTest::Spec
         skip "undecided"
       end
       
-      it "should not append the page number to the last business item on the page"
-      # 2013-06-05, "Business in Grand Committee at 3.45pm", Lord Freud 6
+      it "should not append the page number to the last business item on the page" do
+        # 2013-06-05, "Business in Grand Committee at 3.45pm", Lord Freud 6
+        sitting_day = SittingDay.where(:date => Time.parse("2013-06-05 000:000:00Z")).first
+        block = sitting_day.time_blocks.last
+        block.business_items.first.description.must_equal "1. Mesothelioma Bill [HL] – Committee (Day 1) – Lord Freud"
+      end
     end
   end
 
