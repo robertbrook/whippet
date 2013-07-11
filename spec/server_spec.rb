8d5552941a874944e38523bbf3353fb72830e34a
Robert Brook <mail@robertbrook.com>
2013-07-11 15:29:04 +0100

    adding stub specs before I forget

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -5,21 +5,41 @@ require './server'
 
 class ServerTest < MiniTest::Spec 
  
-  describe "TheServer" do    
+  describe "TheServer" do   
+   
     describe "when asked for an iCal feed at /cal" do
-      it "must generate output" do
-        skip "works locally, not on heroku"
-      end
       
+      it "must generate output"
       
+      it "must generate output with MIME type text/calendar"
+
     end
+    
      describe "when asked for an iCal feed at /cal?ics" do
-      it "must generate output" do
-        skip "works locally, not on heroku"
+      
+      it "must generate output"
+      
+      it "must generate output with MIME type text/calendar"
+      
+     end   
+     
+      describe "when asked for the URL /2013/03/27" do
+      
+        it "should have a link to the PDF"
+        
+        it "should have a link to the Lords calendar"
+      
       end
       
+      describe "when running a search for 'Regulations'" do
+		it "should return these results"      
+      end
       
-    end   
+      describe "when running a search for 'Lord McNally'" do
+		it "should return these results"      
+      end
+    
   end
+  
 end
 
