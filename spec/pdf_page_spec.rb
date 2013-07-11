5133eb66dcdf98c7dcba3500253243e1024a3251
lizconlan <lizconlan@gmail.com>
2013-07-11 16:23:59 +0100

    overhaul of underlying pdf parser

--- spec/pdf_page_spec.rb
+++ spec/pdf_page_spec.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 require './spec/minitest_helper.rb'
 require './lib/pdf_page'
 
@@ -93,6 +95,107 @@ class PdfPageTest < MiniTest::Spec
       
       it "should find 4 fonts" do
         @pdf_page.fonts.count.must_equal 4
+      end
+    end
+  end
+  
+  describe "PdfPage", "when given the Forthcoming Business for 13th March 2013 PDF as FB 2013 03 13.pdf" do
+    before do
+      @@pdf2 ||= PDF::Reader.new("./data/FB 2013 03 13.pdf")
+      @pdf = @@pdf2
+    end
+    
+    describe "when asked to load page 1" do
+      before do
+        @pdf_page = PdfPage.new(@pdf.pages.first)
+      end
+      
+      it "should correctly identify 3 different font variations" do
+        fonts = @pdf_page.fonts
+        fonts.length.must_equal 3
+        fonts[:TT0][:family].must_equal "BookAntiqua"
+        fonts[:TT0][:bold].must_equal false
+        fonts[:TT0][:italic].must_equal false
+        
+        fonts[:TT1][:family].must_equal "BookAntiqua-Bold"
+        fonts[:TT1][:bold].must_equal true
+        fonts[:TT1][:italic].must_equal false
+        
+        fonts[:TT2][:family].must_equal "BookAntiqua-Italic"
+        fonts[:TT2][:bold].must_equal false
+        fonts[:TT2][:italic].must_equal true
+      end
+      
+      it "should return both the original plain text and the html markup for each line" do
+        line = @pdf_page.lines[5]
+        line[:plain].must_equal "                  FORTHCOMING BUSINESS\n"
+        line[:html].must_equal "<b>FORTHCOMING BUSINESS</b>"
+        
+        line = @pdf_page.lines[8]
+        line[:plain].must_equal "                    [Notes about this document are set out at the end]\n"
+        line[:html].must_equal "[<i>Notes about this document are set out at the end</i>]"
+        
+        line = @pdf_page.lines[17]
+        line[:plain].must_equal "1.  Oral questions (30 minutes)\n"
+        line[:html].must_equal "1.  Oral questions (30 minutes)"
+        
+        line = @pdf_page.lines[29]
+        line[:plain].must_equal "Business in Grand Committee at 3.45pm\n"
+        line[:html].must_equal "<b>Business in Grand Committee at 3.45pm</b>"
+        
+        line = @pdf_page.lines[30]
+        line[:plain].must_equal "    No business scheduled\n"
+        line[:html].must_equal "No business scheduled"
+      end
+    end
+    
+    describe "when asked to load page 2" do
+      before do
+        @pdf_page = PdfPage.new(@pdf.pages[1])
+      end
+      
+      it "should correctly identify 3 different font variations" do
+        fonts = @pdf_page.fonts
+        fonts.length.must_equal 3
+      end
+      
+      it "should return both the original plain text and the html markup for each line" do
+        line = @pdf_page.lines[1]
+        line[:plain].must_equal "                             The House is expected not to sit\n"
+        line[:html].must_equal "<i>The House is expected not to sit</i>"
+        
+        line = @pdf_page.lines[3]
+        line[:plain].must_equal "Last day to table amendments for the marshalled list for:\n"
+        line[:html].must_equal "Last day to table amendments for the marshalled list for:"
+        
+        line = @pdf_page.lines[4]
+        line[:plain].must_equal "       Welfare Benefits Up-rating Bill - Report Day 1\n"
+        line[:html].must_equal "<i>Welfare Benefits Up-rating Bill - Report Day 1</i>"
+        
+        line = @pdf_page.lines[42]
+        line[:plain].must_equal "    (3, 4 and 5 expected to be debated together)\n"
+        line[:html].must_equal "(<i>3, 4 and 5 expected to be debated together</i>)"
+      end
+    end
+    
+    describe "when asked to load page 6" do
+      before do
+        @pdf_page = PdfPage.new(@pdf.pages[5])
+      end
+      
+      it "should correctly identify 3 different font variations" do
+        fonts = @pdf_page.fonts
+        fonts.length.must_equal 3
+      end
+      
+      it "should return both the original plain text and the html markup for each line" do
+        line = @pdf_page.lines[1]
+        line[:plain].must_equal "                     House dinner in the Peers’ Dining Room at 7.30pm\n"
+        line[:html].must_equal "<i>House dinner in the Peers’ Dining Room at 7.30pm</i>"
+        
+        line = @pdf_page.lines[7]
+        line[:plain].must_equal "3.  Succession to the Crown Bill – Third Reading – Lord Wallace of Tankerness\n"
+        line[:html].must_equal "3.  Succession to the Crown Bill – Third Reading – Lord Wallace of Tankerness"
       end
     end
   end
