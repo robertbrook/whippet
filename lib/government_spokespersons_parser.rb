fa03aa1d76db654933e724afcbe7fedd9b5e44fc
Robert Brook <mail@robertbrook.com>
2013-12-10 12:27:09 +0000

    back to the parser drawingboard

--- lib/government_spokespersons_parser.rb
+++ lib/government_spokespersons_parser.rb
@@ -2,54 +2,44 @@ require 'rest-client'
 require 'nokogiri'
 require 'active_record'
 require './models/government_spokesperson'
+require 'pp'
 
 class GovernmentSpokespersonsParser
   attr_reader :government_spokespersons, :page
   
   def initialize
     @page = "http://www.lordswhips.org.uk/government-spokespersons"
+    @local_test_file = './data/spokespersons.html'
     @government_spokespersons = []
   end
   
   def parse
-    government_spokespersons = scrape()
-    government_spokespersons.each do |government_spokesperson|
-#       GovernmentSpokesperson.find_or_create_by(:name => government_spokesperson)
-      p government_spokesperson
-    end
+     government_spokespersons = scrape()
+    # government_spokespersons.each do |government_spokesperson|
+#        GovernmentSpokesperson.find_or_create_by(:name => government_spokesperson)
+#     end
   end
 
   def scrape
-    if File.exist?('./data/spokespersons.html')
-      page = Nokogiri::HTML('./data/spokespersons.html')
+    if File.exist?(@local_test_file)
+      page = Nokogiri::HTML(File.open(@local_test_file))
     else
       page = Nokogiri::HTML(RestClient.get(@page))
     end
-
     page.css("div.normalcontent > p").children.each do |line|
-        person = {}
+#       pp line
 
-        if line.text.length > 2
-          
-          if line.name == "strong"
-            remit = line.text.strip
-          end
-          
-          possible_role_and_name = line.text.strip.split(':')
-          if (possible_role_and_name.length > 1)
-            person['remit'] = remit
-            person['role'] = possible_role_and_name[0]
-            person['name'] = possible_role_and_name[1].strip
-          else
-            person['remit'] = remit
-            person['role'] = ''
-            person['name'] = possible_role_and_name[0]
-          end
-          
-          p person
-          
-        end
-        
+      if line.name == 'strong'
+        section = line.children.text
+        p section
+      end      
+      
+      if section
+        p 'in section: ' + section
+      else
+        p 'not in a section: ' + line.text
+      end
+      
     end
        
   end
