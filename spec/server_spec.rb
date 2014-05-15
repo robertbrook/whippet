ee6149a5d9b37a28b9e5d86dc375eba1e8123ba9
Robert Brook <mail@robertbrook.com>
2014-05-15 10:39:19 +0100

    simpler layout

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -58,7 +58,9 @@ describe "The server" do
         
     it "should have a link to the Lords calendar" do 
       pending("requires view for date page in HTML")
+      
       get "/2013-03-27"
+      expect(last_response.body).to match(/href='http:\/\/services.parliament.uk\/calendar\/Lords\/#!\/calendar\/Lords\/MainChamber\/2013\/3\/20\/events\.html'/)
     end
   end
     
