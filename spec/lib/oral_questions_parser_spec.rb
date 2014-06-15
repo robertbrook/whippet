d6fd1b25ef61daf45416b4896c2c713d2145b9eb
Robert Brook <mail@robertbrook.com>
2014-06-15 23:06:30 +0100

    oral question specs

--- spec/lib/oral_questions_parser_spec.rb
+++ spec/lib/oral_questions_parser_spec.rb
@@ -57,11 +57,41 @@ describe OralQuestionsParser do
         expect(@parser.date_sections[2].date_string).to eq("Wednesday 25 June 2014")
       end
 
-      xit "should return nothing when asked for the third date section'" do 
+      xit "should return nothing when asked for the fifth date section'" do 
         @parser.scrape()
-        expect(@parser.date_sections[2].date_string).to eq("Wednesday 25 June 2014")
+        expect(@parser.date_sections[4]).to eq(nil)
+      end
+
+      xit "should return the fourth date section with 3 items" do 
+        @parser.scrape()
+        expect(@parser.date_sections[3].length).to eq(3)
+      end
+
+      xit "should return the coplete first question of the fourth date section" do 
+        @parser.scrape()
+        expect(@parser.date_sections[3].questions[0].complete).to eq("Baroness Masham of Ilton to ask Her Majesty’s Government what steps they will take to help to remove barriers to access to secondary care for symptomatic patients so they are identified and can start treatment earlier. Earl Howe (Department of Health).")
+      end
+
+      xit "should return the questioner of the first question of the fourth date section as 'Baroness Masham of Ilton'" do 
+        @parser.scrape()
+        expect(@parser.date_sections[3].questions[0].questioner).to eq("Baroness Masham of Ilton")
+      end
+
+      xit "should return the text of the first question of the fourth date section as 'Baroness Masham of Ilton'" do 
+        @parser.scrape()
+        expect(@parser.date_sections[3].questions[0].text).to eq("to ask Her Majesty’s Government what steps they will take to help to remove barriers to access to secondary care for symptomatic patients so they are identified and can start treatment earlier.")
       end
       
+      xit "should return the answerer of the first question of the fourth date section as 'Earl Howe'" do 
+        @parser.scrape()
+        expect(@parser.date_sections[3].questions[0].answerer).to eq("Earl Howe")
+      end
+
+      xit "should return the department of the first question of the fourth date section as 'Earl Howe'" do 
+        @parser.scrape()
+        expect(@parser.date_sections[3].questions[0].department).to eq("Department of Health")
+      end
+
       # it "should return a list of oral questions in oral_questions including the section 'Leader of the House of Lords and Chancellor of the Duchy of Lancaster'" do 
       #   @parser.scrape()
       #   expect(@parser.government_sections).to include("Leader of the House of Lords and Chancellor of the Duchy of Lancaster")
