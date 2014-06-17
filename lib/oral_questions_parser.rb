3de673f493ae6379e865317f0030782137196456
Robert Brook <robertbrook@fastmail.fm>
2014-06-17 17:50:42 +0100

    parsing by regex

--- lib/oral_questions_parser.rb
+++ lib/oral_questions_parser.rb
@@ -1,37 +1,9 @@
 require 'rest-client'
 require 'nokogiri'
 require 'date'
-
 require "active_record"
 require "./models/oral_question.rb"
 
-class OralQuestionsDocument < Nokogiri::XML::SAX::Document
-  def xmldecl(version, encoding, standalone)
-  end
-  def start_document
-  end
-  def end_document
-  end
-  def start_element(name, attrs = [])
-    puts "#{name} started with attrs #{attrs.inspect}"
-  end
-  def end_element(name)
-  end
-  def start_element_namespace(name, attrs = [], prefix = nil, uri = nil, ns = [])
-  end
-  def end_element_namespace(name, prefix = nil, uri = nil)
-  end
-  def characters(string)
-  end
-  def comment(string)
-  end
-  def warning(string)
-  end
-  def error(string)
-  end
-  def cdata_block(string)
-  end
-end
 
 class OralQuestionsParser
   attr_reader :page, :oral_questions, :title, :date_sections
@@ -67,49 +39,37 @@ class OralQuestionsParser
   #   end
 
   def scrape
+    @oral_questions = {}
     response = RestClient.get(@page)
-    parser = Nokogiri::HTML::SAX::Parser.new(OralQuestionsDocument.new)
-    parser.parse(response.body)
+
+    @oral_questions['title'] = ""
+    @oral_questions['dates'] = []
+    @oral_questions['questions'] = []
+    response.body.each_line do |line|
+
+      case line
+      when /Week beginning (.*)<\/strong>/
+          @oral_questions['title'] = $1
+          # @oral_questions['state'] = $1
+
+      when /<p class="txt666 txtbold">(.*)<\/p>/
+          @oral_questions['dates'] << $1
+          # @oral_questions['state'] = $1
+
+      when /<p>(.*)<\/p>/
+          puts $1
+          @oral_questions['questions'] << [@oral_questions['dates'][-1], $1]
+          # @oral_questions['state'] = $1
+
+      else
+          ""
+      end
+      
+    end
+
+    puts @oral_questions.to_yaml
+
   end
-       # reader = Nokogiri::XML::Reader
 
-#     year = ""
-#     name = ""
-#     paras.each do |para|
-#       para.children.each do |node|
-#         case node.name
-#         when "strong"
-#           if node.text.strip =~ /^(\d{4})$/
-#             year = $1
-#           end
-#         when "em"
-#           name = node.text
-#         when "text"
-#           dates = extract_recess_days(node.text, year)
-#           unless dates.empty?
-#             @recesses << {:name => name, :start_date => dates[0], :finish_date => dates[1]}
-#           end
-#         end
-#       end
-#     end
-#     @recesses
-
-  
-#   private
-  
-#   def extract_recess_days(text, year)
-#     days = []
-#     if text =~ /([A-Z][a-z]+day\s+\d+\s+[A-Z][a-z]+(?:\s+\d{4})?)\s+to\s+([A-Z][a-z]+day\s+\d+\s+[A-Z][a-z]+(?:\s+\d{4})?)/
-#       days << append_year(year, $1)
-#       days << append_year(year, $2)
-#     end
-#     days
-#   end
-  
-#   def append_year(year, string)
-#     unless string.strip =~ /\d{4}$/
-#       string = "#{string} #{year}"
-#     end
-#     string
-#   end
 end
+       
